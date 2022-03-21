05969a2dbb1190f0eda17b68dbc60c04ac226ccf
Jeffrey <chunjuwang@163.com>
2022-03-21 20:31:06 +0800

    go on

--- index.js
+++ index.js
@@ -5,10 +5,23 @@ Array.prototype.flatCustom = function (depth) {
 
     Number.isNaN(Number(depth)) && (depth = 0);
 
+    console.log(depth);
+
+    for (let i = 0; i < this.length; i++) {
+        if (Object.prototype.toString.call(this[i]) === '[object Array]' && depth > 0) {
+            this[i].flatCustom(depth - 1).forEach(function (v) {
+                container.push(v);
+            });
+        } else {
+            container.push(this[i]);
+        }
+    }
+
     return container;
 }
 
 let arr = [1, [2, [5, 6, [99]]]];
 
-console.log(arr.flat(null));
+console.log(arr.flatCustom(1));
+console.log(arr.flat(0));
 console.log(arr);
